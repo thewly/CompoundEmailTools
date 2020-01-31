c2218ed30e28261b9fc15073bc61be97538d8380
Cole <mcole@d-mcole-mbp2.local>
2020-01-31 08:18:47 -0600

    added the EESSKSRO email to the site

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import EE_SS_GI_AE from './Assets/EmailTemplates/EE_SS_GI_AE';
 // import EE_SS_GI_NAE_Annc from './Assets/EmailTemplates/EE_SS_GI_NAE_Annc';
 // import EE_SS_GI_NAE from './Assets/EmailTemplates/EE_SS_GI_NAE';
 import EE_SS_RO from './Assets/EmailTemplates/EE_SS_RO';
+import EE_SS_KS_RO from './Assets/EmailTemplates/EE_SS_KS_RO';
 
 import InputGeneric from './components/InputGeneric/InputGeneric';
 import EESSRO_Body_Preview from './components/EESSRO_Body_Preview/EESSRO_Body_Preview';
@@ -27,11 +28,13 @@ class App extends Component {
   state = {
     schoolLink: "schoollink",
     headline: "It's time to order your Official [School name] Ring!",
-    headlineEESSGIAE: "Order your Official [School Name] Ring and take your graduation photos!",
+    EESSKSRO_Headline: "Free Kendra Scott Gift with Purchase* of your Official [School name] Ring.",
+    EESSGIAE_Headline: "Order your Official [School Name] Ring and take your graduation photos!",
     inHandDate: "111111",
     SMI: "222222",
     bodyCopy: "Be part of a timeless [Nickname] tradition. Order your personalized ring to show your pride and celebrate your success with fellow classmates.",
-    bodyCopyEESSGIAE: "Be part of a timeless [Nickname] tradition. Visit Balfour and GradImages on-campus during the event.",
+    EESSGIAE_BodyCopy: "Be part of a timeless [Nickname] tradition. Visit Balfour and GradImages on-campus during the event.",
+    EESSKSRO_BodyCopy: "Your college ring serves as an enduring, lifelong reminder of your achievement. Now you can make your celebration even more exciting with this <strong>limited-time offer.</strong> With your qualifying purchase, receive your choice of one of three Kendra Scott Ever Necklaces (a $65 value). ",
     buttonColor: "#000000",
     footerColor: "000000",
     subhead: "ONE DAY ONLY",
@@ -57,13 +60,14 @@ class App extends Component {
     showEESSROEmail: false,
     showEESS2ColEmail: false,
     showEESSGIAEEmail: false,
+    showEESSKSROEmail: false,
 
     showPageHeader:true,
 
     oneDTP: true
   }
   showPageHeader = () => {
-    if (this.state.showEESS2ColEmail || this.state.showEESSGIAEEmail || this.state.showEESSROEmail) {
+    if (this.state.showEESS2ColEmail || this.state.showEESSGIAEEmail || this.state.showEESSROEmail || this.state.showEESSKSROEmail) {
       this.setState ({
         showPageHeader:false
       })
@@ -127,6 +131,8 @@ class App extends Component {
       [event.target.name]: !doesShow
     });
   }
+
+  // these are the functions that actually put the form into the HTML emails
   MakeEE_SS_RO = () => {
     var result = EE_SS_RO;
 
@@ -156,6 +162,36 @@ class App extends Component {
     result = result.replace('Location', this.state.location);
 
     download(result, 'XXXX_EE-SS-RO.html', "text/html");
+  }
+  MakeEE_SS_KS_RO = () => {
+    var result = EE_SS_KS_RO;
+
+    // these items happen more than once & require regex
+    var Cregex = /#000000/gi;
+    var SNregex = /schoolname/gi;
+    var SLregex = /schoollink/gi;
+    var UTMregex = /111111/gi;
+    var SMIregex = /222222/gi;
+
+    // this swaps out what's in the email for what's in the finished form
+    result = result.replace(Cregex, this.state.buttonColor);
+    result = result.replace(SNregex, this.state.schoolName);
+    result = result.replace(SLregex, this.state.schoolLink);
+    result = result.replace(UTMregex, this.state.inHandDate);
+    result = result.replace(SMIregex, this.state.SMI);
+    result = result.replace('Free Kendra Scott Gift with Purchase* of your Official [School name] Ring.', this.state.EESSKSRO_Headline);
+    result = result.replace('Your college ring serves as an enduring, lifelong reminder of your achievement. Now you can make your celebration even more exciting with this <strong>limited-time offer.</strong> With your qualifying purchase, receive your choice of one of three Kendra Scott Ever Necklaces (a $65 value). ', this.state.EESSKSRO_BodyCopy);
+    result = result.replace('ONE DAY ONLY', this.state.subhead);
+    result = result.replace('Day, Month #', this.state.weekdayMonthDate);
+    result = result.replace('00 a.m.', this.state.startTime);
+    result = result.replace('00 p.m.', this.state.endTime);
+    if (!this.state.oneDTP){
+      result = result.replace('Location<br>', this.state.weekdayMonthDate2 + '<br>Location<br>');
+      result = result.replace('Location<br>', this.state.startTime2 + ' &ndash; ' + this.state.endTime2 + '<br>Location<br>');
+    }
+    result = result.replace('Location', this.state.location);
+
+    download(result, 'XXXX_EE-SS-KS-RO.html', "text/html");
   }
   MakeEE_SS_2Col = () => {
     var result = EE_SS_2Col;
@@ -229,6 +265,7 @@ class App extends Component {
   render() {
     let EmailEditor = null;
 
+    // these are the emails that will render when you click the buttons int he header
     if (this.state.showEESSROEmail) {
       EmailEditor =
         <div className="row">
@@ -593,15 +630,15 @@ class App extends Component {
               <InputGeneric 
                 headline="Headline:"
                 change={this.valueChangeHandler}
-                value={this.state.headlineEESSGIAE}
-                name="headlineEESSGIAE"
+                value={this.state.EESSGIAE_Headline}
+                name="EESSGIAE_Headline"
                 rows="2"
               />
               <InputGeneric 
                 headline="Body Copy:"
                 change={this.valueChangeHandler}
-                value={this.state.bodyCopyEESSGIAE}
-                name="bodyCopyEESSGIAE"
+                value={this.state.EESSGIAE_BodyCopy}
+                name="EESSGIAE_BodyCopy"
                 rows="3"
               />
               <InputGeneric 
@@ -725,8 +762,8 @@ class App extends Component {
               click={this.SchoolLinkPreview}
             />
             <EESSGIAE_Body_Preview 
-              headline={this.state.headlineEESSGIAE}
-              bodyCopy={this.state.bodyCopyEESSGIAE}
+              headline={this.state.EESSGIAE_Headline}
+              bodyCopy={this.state.EESSGIAE_BodyCopy}
               subhead={this.state.subhead}
               weekdayMonthDate={this.state.weekdayMonthDate}
               startTime={this.state.startTime}
@@ -763,18 +800,177 @@ class App extends Component {
           </div>
         </div>
     }
+    if (this.state.showEESSKSROEmail) {
+      EmailEditor =
+        <div className="row">
+          <div className="col-md-6 text-center">
+            <div className="form-group">
+              <InputGeneric 
+                headline="School Name:"
+                change={this.valueChangeHandler}
+                value={this.state.schoolName}
+                name="schoolName"
+                rows="1"
+              />
+              <InputGeneric 
+                headline="Headline:"
+                change={this.valueChangeHandler}
+                value={this.state.EESSKSRO_Headline}
+                name="EESSKSRO_Headline"
+                rows="2"
+              />
+              <InputGeneric 
+                headline="Body Copy:"
+                change={this.valueChangeHandler}
+                value={this.state.EESSKSRO_BodyCopy}
+                name="EESSKSRO_BodyCopy"
+                rows="3"
+              />
+              {/* <InputGeneric 
+                headline="Button Color:"
+                change={this.valueChangeHandler}
+                value={this.state.buttonColor}
+                name="buttonColor"
+                rows="1"
+              /> */}
+              <InputGeneric 
+                headline="School Link:"
+                change={this.valueChangeHandler}
+                value={this.state.schoolLink}
+                name="schoolLink"
+                rows="1"
+              />
+              <InputGeneric 
+                headline="Subhead:"
+                change={this.valueChangeHandler}
+                value={this.state.subhead}
+                name="subhead"
+                rows="1"
+              />
+              <InputGeneric 
+                headline="Date:"
+                change={this.valueChangeHandler}
+                value={this.state.weekdayMonthDate}
+                name="weekdayMonthDate"
+                rows="1"
+              />
+              <InputGeneric 
+                headline="Start Time:"
+                change={this.valueChangeHandler}
+                value={this.state.startTime}
+                name="startTime"
+                rows="1"
+              />
+              <InputGeneric 
+                headline="End Time:"
+                change={this.valueChangeHandler}
+                value={this.state.endTime}
+                name="endTime"
+                rows="1"
+              />
+              
+              <div className={styles.space}>
+              <button className="btn-sm btn-secondary" onClick={this.changeToTwoDTP}>2 DTP Toggle</button>
+              </div>
+              {this.state.oneDTP ? null : (
+                <div>
+                  <InputGeneric 
+                    headline="2nd Date:"
+                    change={this.valueChangeHandler}
+                    value={this.state.weekdayMonthDate2}
+                    name="weekdayMonthDate2"
+                    rows="1"
+                  />
+                  <InputGeneric 
+                    headline="2nd Start Time:"
+                    change={this.valueChangeHandler}
+                    value={this.state.startTime2}
+                    name="startTime2"
+                    rows="1"
+                  />
+                  <InputGeneric 
+                    headline="2nd End Time:"
+                    change={this.valueChangeHandler}
+                    value={this.state.endTime2}
+                    name="endTime2"
+                    rows="1"
+                  />
+                </div>
+              )}
+              <InputGeneric 
+                headline="Location:"
+                change={this.valueChangeHandler}
+                value={this.state.location}
+                name="location"
+                rows="1"
+              />
+              <InputGeneric 
+                headline="SMI:"
+                change={this.valueChangeHandler}
+                value={this.state.SMI}
+                name="SMI"
+                rows="1"
+              />
+              <InputGeneric 
+                headline="In Hand Date:"
+                change={this.valueChangeHandler}
+                value={this.state.inHandDate}
+                name="inHandDate"
+                rows="1"
+              />
+              <button className="btn btn-secondary" onClick={this.MakeEE_SS_RO}>Make Email</button>
+            </div>
+          </div>
+          <div className="col-md-6 text-center">
+            <ImageHeroPreview
+              schoolName={this.state.schoolName}
+              click={this.SchoolLinkPreview}
+            />
+            <EESSRO_Body_Preview
+              headline={this.state.EESSKSRO_Headline}
+              bodyCopy={this.state.EESSKSRO_BodyCopy}
+              font="white"
+              click={this.SMITestButton}
+              buttonColor={this.state.buttonColor}
+              subhead={this.state.subhead}
+              weekdayMonthDate={this.state.weekdayMonthDate}
+              startTime={this.state.startTime}
+              endTime={this.state.endTime}
+              location={this.state.location}
+              oneDTP={this.state.oneDTP}
+              weekdayMonthDate2={this.state.weekdayMonthDate2}
+              startTime2={this.state.startTime2}
+              endTime2={this.state.endTime2}
+            />
+            <h5 className={styles.bulletTitle}>Each Ring Includes:</h5>
+            <ul className={styles.bulletList}>
+              <li>Optional interest-free payment plans</li>
+              <li>A lifetime warranty with free resizing, polishing and cleaning</li>
+              <li>A Ring Loss Protection Plan</li>
+            </ul>
+            <ImagePanelPreview
+              schoolName={this.state.schoolName}
+              click={this.SchoolLinkPreview}
+            />
+            <p className={styles.TermsAndConditions}>
+            *To qualify for the FREE Kendra Scott gift promotion, a single class ring or necklace product order must total $399 (or more) before shipping or tax. This offer applies to Select Colleges, Select High Schools and Select Ring Styles, Liz James Necklaces and the Journey Collection purchase only. Offer valid 11:59 CST on 12/12/19 and ends 11:59pm on 05/31/20 or while supplies last. No promotional code needed online. Certain product exclusions may apply. This promotion cannot be applied to previous purchases and cannot be redeemed for cash, or used in combination with any other discount, offer or promotion. Terms of offer are subject to change. Other restrictions may apply.
+            </p>
+          </div>
+        </div>
+    }
 
     return (
       <div className="container">
         <div className="row">
           <div className="col text-center MainNav">
-            {this.state.showEESS2ColEmail || this.state.showEESSGIAEEmail || this.state.showEESSROEmail ? (null) : (
+            {this.state.showEESS2ColEmail || this.state.showEESSGIAEEmail || this.state.showEESSROEmail || this.state.showEESSKSROEmail ? (null) : (
             <Animated animationIn="fadeIn" animationOut="fadeOut" isVisible={true}>
               <h3 className={styles.rudeNav}>Balfour Email Maker 2.0</h3>
             </Animated>
             )}
               
             <button className="btn btn-secondary m-2" name="showEESSROEmail" onClick={this.toggleEmailHandler}>EE-SS-RO</button>
+            <button className="btn btn-secondary m-2" name="showEESSKSROEmail" onClick={this.toggleEmailHandler}>EE-SS-KS-RO</button>
             <button className="btn btn-secondary" name="showEESS2ColEmail" onClick={this.toggleEmailHandler}>EE-SS-2Col</button>
             <button className="btn btn-secondary m-2" name="showEESSGIAEEmail" onClick={this.toggleEmailHandler}>EE-SS-GI-AE</button>
           </div>
